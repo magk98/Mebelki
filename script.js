ee23ebca72f3695a5e0760ce6d370c4be0af829a
magk98 <magk98@poczta.fm>
2022-01-17 22:58:16 +0100

    changing colors

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function changeColor() {
-    if (document.getElementById('fotel__MA_Body').getAttribute('diffuseColor') !== '1 0 0')
-        document.getElementById('fotel__MA_Body').setAttribute('diffuseColor', '1 0 0');
-    else
-        document.getElementById('fotel__MA_Body').setAttribute('diffuseColor', '0 0 0');
+function changeColor(color) {
+        document.getElementById('fotel__MA_Body').setAttribute('diffuseColor', color);
 }
