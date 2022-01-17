8510f58d5efe50d757435ec4321038fd1b5cd7c2
Piotr Matląg <piotr.matlag@protonmail.com>
2022-01-17 22:12:13 +0100

    Add TailwindCSS and Vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 class="text-3xl font-bold">Hello!</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
