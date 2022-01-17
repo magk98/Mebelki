dd8ff179212a5e82b1dbef5ea2cce4acce9db665
magk98 <magk98@poczta.fm>
2022-01-18 00:15:59 +0100

    added chair

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function changeColor(color) {
-        document.getElementById('fotel__MA_Body').setAttribute('diffuseColor', color);
+function changeColor(element, color) {
+        // const elementId = element + '__MA_Body'; //dla fotela
+        const elementId = element + '__MA_lambert2'; //dla krzesla
+
+        document.getElementById(elementId).setAttribute('diffuseColor', color);
 }
