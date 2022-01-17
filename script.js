5386f5969a08f04a0037bb8d33e14ca12952e181
magk98 <magk98@poczta.fm>
2022-01-17 22:36:22 +0100

    refactor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function redNose() {
-    if (document.getElementById('Deer__MA_Nose').getAttribute('diffuseColor') !== '1 0 0')
-        document.getElementById('Deer__MA_Nose').setAttribute('diffuseColor', '1 0 0');
+function changeColor() {
+    if (document.getElementById('fotel__MA_Body').getAttribute('diffuseColor') !== '1 0 0')
+        document.getElementById('fotel__MA_Body').setAttribute('diffuseColor', '1 0 0');
     else
-        document.getElementById('Deer__MA_Nose').setAttribute('diffuseColor', '0 0 0');
+        document.getElementById('fotel__MA_Body').setAttribute('diffuseColor', '0 0 0');
 }
