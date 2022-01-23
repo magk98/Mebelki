c9e026f86d308729d91f95bb5dcd813a6773f4df
Piotr Matląg <piotr.matlag@protonmail.com>
2022-01-23 23:59:33 +0100

    Specify rotation axis

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,7 +3,7 @@
     <div class="rounded-lg drop-shadow-md p-4 m-4 bg-gray-100 border transition-all border-solid border-vue-green hover:border-4 flex flex-col items-center">
       <x3d showLog="true" class="w-72 h-80 border border-solid border-vue-green rounded-xl">
         <scene>
-          <transform :DEF="`product-preview-transform-${name}`">
+          <transform DEF="product-preview-transform">
             <shape>
               <appearance>
                   <material diffuseColor='0.3 0 0'></material>
@@ -12,9 +12,9 @@
             </shape>
           </transform>
           <timeSensor DEF="time" cycleInterval="2" loop="true"> </timeSensor>
-          <orientationInterpolator DEF='spin' key='0 0.25 0.5 0.75 1' keyValue='0 0 0 0  0 0 0 1.57079  0 0 0 3.14159  0 0 0 4.71239  0 0 0 6.28317'></orientationInterpolator> 
+          <orientationInterpolator DEF='spin' key='0 0.25 0.5 0.75 1' keyValue='0 1 0 0  0 1 0 1.57079  0 1 0 3.14159  0 1 0 4.71239  0 1 0 6.28317'></orientationInterpolator> 
           <route fromNode="time" fromField="fraction_changed" toNode="spin" toField="set_fraction"></route> 
-          <route fromNode="spin" fromField="value_changed" :toNode="`product-preview-transform-${name}`" toField="set_rotation"> </route>
+          <route fromNode="spin" fromField="value_changed" toNode="product-preview-transform" toField="set_rotation"> </route>
         </scene>
       </x3d>
       {{ name }}
