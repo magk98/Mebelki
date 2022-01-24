3db39691cbc288bb7d020d624dea9da6a4138cc2
MarcinMichna <marcinxmichna@gmail.com>
2022-01-24 20:20:33 +0100

    models rotation preview

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,15 +4,10 @@
       <x3d class="w-72 h-80 border border-solid border-vue-green rounded-xl">
         <scene>
           <transform DEF="product-preview-transform">
-            <shape>
-              <appearance>
-                  <material diffuseColor='0.3 0 0'></material>
-              </appearance>
-              <box></box>
-            </shape>
+            <Inline nameSpaceName="item" mapDEFToID="true" onclick='changeColor();' :url="`models/${name}.x3d`"></Inline>-->
           </transform>
           <timeSensor DEF="time" cycleInterval="6" loop="true"> </timeSensor>
-          <orientationInterpolator DEF='spin' Key='0 0.25 0.5 0.75 1' keyValue='0 1 0 0  0 1 0 1.57079  0 1 0 3.14159  0 1 0 4.71239  0 1 0 6.28317'></orientationInterpolator> 
+          <orientationInterpolator DEF='spin' Key='0 0.25 0.5 0.75 1' keyValue='0 1 0 0  0 1 0 1.57079  0 1 0 3.14159  0 1 0 4.71239  0 1 0 6.28317'></orientationInterpolator>
           <route fromNode="time" fromField="fraction_changed" toNode="spin" toField="set_fraction"></route> 
           <route fromNode="spin" fromField="value_changed" toNode="product-preview-transform" toField="set_rotation"> </route>
         </scene>
